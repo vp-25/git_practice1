7376d813a43ea91ac5917800010db9f6964ef0f2
vp-25 <vishnupriyakonadti@gmail.com>
2025-07-15 07:43:54 +0530

    push

---
+++ src/test/java/sample/firstprogram.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class firstprogram {
+	public static void main(String [] args){
+		System.out.println("Hello everyone");
+		System.out.println("Welcome");
+		
+		//push2
+	}
+
+}
