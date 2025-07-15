3a613a223252b2f96655446d6a64ba43516cc035
vp-25 <vishnupriyakonadti@gmail.com>
2025-07-15 07:48:56 +0530

    branching

--- src/test/java/sample/firstprogram.java
+++ src/test/java/sample/firstprogram.java
@@ -2,10 +2,11 @@ package sample;
 
 public class firstprogram {
 	public static void main(String [] args){
+		//i'm branching
 		System.out.println("Hello everyone");
 		System.out.println("Welcome");
 		
-		//pull
+		
 	}
 
 }
